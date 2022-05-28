95fb83208d2fae25e89ee2880b909695bdbccbc2
Ceyhun <contact.ceyhun@gmail.com>
2022-05-29 02:41:58 +0400

    added new project

---
+++ Exchange rate calculation program/Exchange_rate_calculation_program/Exchange_rate_calculation_program/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+int sumAzn = 10000;
+float sumDollar = 0.59f;
+float sumEuro = 0.55f;
+float sumRub = 39.14f;
+
+float totalDollar = sumAzn * sumDollar;
+float totalEuro = sumAzn * sumEuro;
+float totalRub = sumAzn * sumRub;
+
+string textsumAzn = "Mebleg (AZN) : ";
+Console.WriteLine(textsumAzn);
+Console.WriteLine(sumAzn);
+
+string textsumDollar = "ABŞ dollari (USD) :";
+Console.WriteLine(textsumDollar);
+Console.WriteLine(totalDollar);
+
+string textsumEuro = "Avro (EUR) :";
+Console.WriteLine(textsumEuro);
+Console.WriteLine(totalEuro);
+
+string textsumRub = "Rusiya rublu (RUB) :";
+Console.WriteLine(textsumRub);
+Console.WriteLine(totalRub);
+
+Console.ReadLine();
