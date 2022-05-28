21d2219c7f74254de0e0804f5f8c741a648808b5
Ceyhun <contact.ceyhun@gmail.com>
2022-05-29 02:52:16 +0400

    added second proqram

---
+++ Proqram for calculating cargo orders/cargoProqram/cargoProqram/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+//1.Adidas - 4 (price:200$) 2.Gucci - 2(price:1000$) 3.Nike - 1(price:150$)
+
+int shoesAdidas = 4;
+int adidasPrice = 200;
+int sumAdidas = shoesAdidas * adidasPrice;
+
+int shoesGucci = 2;
+int gucciPrice = 1000;
+int sumGucci = shoesGucci * gucciPrice;
+
+int shoesNike = 1;
+int nikePrice = 150;
+int sumNike = shoesNike * nikePrice;
+
+string textFirmaA1 = "Firma : ";
+Console.WriteLine(textFirmaA1);
+
+string textFirmaA2 = "Adidas ";
+Console.WriteLine(textFirmaA2);
+
+string textFirmaA3 = "Ayaqqabi sayi :";
+Console.WriteLine(textFirmaA3);
+
+
+Console.WriteLine(shoesAdidas);
+
+string textFirmaA5 = "Birinin qiyməei ($) :";
+Console.WriteLine(textFirmaA5);
+
+
+Console.WriteLine(adidasPrice);
+
+
+string textFirmaA7 = "Hamisinin birlikde qiymeti ($) : ";
+Console.WriteLine(textFirmaA7);
+
+Console.WriteLine(sumAdidas);
+Console.WriteLine("");
+
+string textFirmaB1 = "Firma : ";
+Console.WriteLine(textFirmaB1);
+
+string textFirmaB2 = "Gucci ";
+Console.WriteLine(textFirmaB2);
+
+string textFirmaB3 = "Ayaqqabi sayi :";
+Console.WriteLine(textFirmaB3);
+
+Console.WriteLine(shoesGucci);
+
+string textFirmaB5 = "Birinin qiyməei ($) :";
+Console.WriteLine(textFirmaB5);
+
+Console.WriteLine(gucciPrice);
+
+
+string textFirmaB7 = "Hamisinin birlikde qiymeti ($) : ";
+Console.WriteLine(textFirmaB7);
+
+Console.WriteLine(sumGucci);
+Console.WriteLine("");
+
+string textFirmaC1 = "Firma : ";
+Console.WriteLine(textFirmaC1);
+
+string textFirmaC2 = "Nike ";
+Console.WriteLine(textFirmaC2);
+
+string textFirmaC3 = "Ayaqqabi sayi :";
+Console.WriteLine(textFirmaC3);
+
+Console.WriteLine(shoesNike);
+
+string textFirmaC5 = "Birinin qiyməei ($) :";
+Console.WriteLine(textFirmaC5);
+
+
+Console.WriteLine(nikePrice);
+
+
+string textFirmaC7 = "Hamisinin birlikde qiymeti ($) : ";
+Console.WriteLine(textFirmaC7);
+
+Console.WriteLine(sumNike);
+Console.WriteLine("");
+
+string textSum = "Yekun xerclenen mebleq :";
+Console.WriteLine(textSum);
+
+int sumAll = sumAdidas + sumGucci + sumNike;
+Console.WriteLine(sumAll);
